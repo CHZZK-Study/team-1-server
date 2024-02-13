e7eb6c3d9e4ffa34a266ffe5cae85014b63e7fa1
anneu <b.imnuj@gmail.com>
2024-02-13 22:16:16 +0900

    chore : 프로젝트 생성

---
+++ src/main/java/foxcord/FoxcordApplication.java
@@ -0,0 +1,13 @@
+package foxcord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoxcordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoxcordApplication.class, args);
+    }
+
+}
---
+++ src/test/java/foxcord/FoxcordApplicationTests.java
@@ -0,0 +1,13 @@
+package foxcord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoxcordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
