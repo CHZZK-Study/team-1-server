3f8402bad90792d06d80b850e5702117599dcef0
wonjun <wonjon0930@daum.net>
2024-02-26 22:09:43 +0900

    refactor : 그룹 도메인 및 서비스 테스트 코드 분리, 오탈자 수정

---
+++ src/test/java/foxcord/domain/group/entity/GroupTest.java
@@ -0,0 +1,38 @@
+package foxcord.domain.group.entity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import foxcord.group.entity.Group;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GroupTest {
+
+    @Test
+    @DisplayName("그룹 이름을 통해 그룹을 생성한다.")
+    void generateGroup() {
+        // given, when
+        Group group = new Group("fxGroup");
+
+        // then
+        assertThat(group).isInstanceOf(Group.class);
+        assertThat(group.getName()).isEqualTo("fxGroup");
+    }
+
+    @Test
+    @DisplayName("주어진 그룹 이름으로 그룹을 수정한다.")
+    void updateGroup() {
+        // given
+        Group group = new Group("멋쟁이그룹");
+        String updateGroupName = "정말멋쟁이그룹";
+
+        // when
+        group.update(updateGroupName);
+
+        // then
+        assertThat(group.getName()).isEqualTo(updateGroupName);
+    }
+
+}
